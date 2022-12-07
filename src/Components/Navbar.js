79303097297461217c598d08701dadd761c943b0
Franchesca <ihuoma.franchesca@gmail.com>
2022-12-07 18:24:49 +0100

    edited screens for a better functionality

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,14 +15,14 @@ const Navbar = () => {
   return (
     <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
         <div>
-            <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
+           <a href='/'><h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1></a>
         </div>
         <ul className='hidden md:flex'>
-<li>Home</li>
-<li>Destinations</li>
-<li>Travel</li>
-<li>Views</li>
-<li>Book</li>
+<a href='/'><li>Home</li></a>
+<a href='/'><li>Destinations</li></a>
+<a href='/'><li>Travel</li></a>
+<a href='/'><li>Views</li></a>
+<a href='/'><li>Book</li></a>
         </ul>
         <div className='hidden md:flex'>
             <BiSearch size={20} className='mr-4' />
@@ -36,11 +36,11 @@ const Navbar = () => {
   <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
   <ul>
     <h1>BEACHES.</h1>
-<li className='border-b'>Home</li>
-<li className='border-b'>Destinations</li>
-<li className='border-b'>Travel</li>
-<li className='border-b'>Views</li>
-<li className='border-b'>Book</li>
+<a href='/'><li className='border-b'>Home</li></a>
+<a href='/'><li className='border-b'>Destinations</li></a>
+<a href='/'><li className='border-b'>Travel</li></a>
+<a href='/'><li className='border-b'>Views</li></a>
+<a href='/'><li className='border-b'>Book</li></a>
 <div className='flex flex-col'>
   <button className='my-6'>Search</button>
   <button>Account</button>
--- src/Components/SelectsCards.jsx
+++ src/Components/SelectsCards.jsx
@@ -5,7 +5,7 @@ const SelectsCards = (props) => {
     <div className='relative'>
     <img src={props.bg} alt='/' className='w-full h-full object-cover' />
     <div className='absolute top-0 left-0 bg-gray-900/30 w-full h-full'>
-        <p className='absolute text-2xl left-4 bottom-4 text-white font-bold'>{props.text}</p>
+        <a href="https://www.tripadvisor.com/AttractionProductReview-g150809-d11449893-Cozumel_Submarine_Expedition-Cozumel_Yucatan_Peninsula.html" className='absolute text-2xl left-4 bottom-4 text-white font-bold hover:text-slate-500 hover:text-3xl'>{props.text}</a>
     </div>
             </div>
   )
